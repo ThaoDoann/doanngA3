3c0096c1a5e68ad622b85065af18c7d1cf7ca55d
Thao Doan <43444987+ThaoDoann@users.noreply.github.com>
2019-07-26 15:33:12 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,10 @@
+import { books } from "../../app/bookInfo";
+
+export const MYBOOKS: books[] = [
+    { bookName: "Wuthering Heights", author: "Emily Brontë", bookGenre: "Novel", yearPublished: "1847", picture: "assets/images/WutheringHeights.jpg" },
+    { bookName: "The Da Vinci Code", author: "Dan Brown", bookGenre: "Mystery", yearPublished: "2003", picture: "assets/images/DaVinciCode.jpg" },
+    { bookName: "Alice's Adventures in Wonderland", author: "Lewis Carroll", bookGenre: "Literary nonsense", yearPublished: "1865", picture: "assets/images/Language_of_god_francis_collins.jpg"},
+    { bookName: "Twilight", author: "Stephenie Meyer", bookGenre: "romance", yearPublished: "2005", picture: "assets/images/Twilightbook.jpg" }
+]
+
+
